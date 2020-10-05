24ec04cf8f267c880981cb31eac2115a8254446f
TraderBagel <traderbagel@gmail.com>
2020-10-05 22:39:15 +0800

    update scala playground

--- scala/src/playground/ScalaPlayground.scala
+++ scala/src/playground/ScalaPlayground.scala
@@ -1,5 +1,8 @@
 package playground
 
-class ScalaPlayground {
+object ScalaPlayground extends App {
+  println("Hello, Scala!")
 
+  val aNumberString = 424242
+  println("abc" :+ aNumberString.toString) // prepending, appending
 }
