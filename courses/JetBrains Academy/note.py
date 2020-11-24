9b03d586fd5599512a23db77cfd6ff70dedd1e31
traderbagel <traderbagel@gmail.com>
2020-11-24 23:21:38 +0800

    save notes | seperate work and home

---
+++ courses/JetBrains Academy/note.py
@@ -0,0 +1,15 @@
+print(b'123')  # bits: lowercase
+print(B'123')  # bytes: capital
+
+'''
+a byte is always 8 bit
+IEC Standard:
+10^6 (10^3 * 10^3) MB (power of 10)
+2^20 (2^10 * 2^10) MiB (power of 2)
+B Here Means `Byte`
+
+Examples:
+16384 bits = (16384 / 8) / 1024 = 2 KiB
+1 GiB = 1 * 1024 * 1024 = 1048576 KiB = 1048576 * 8 bits
+1 GB = 1 * 1000 * 1000 = 1000000 kB
+'''
